12faea3505a58d1b5c5eec42bc33c57d014ce158
Waqas <wmunir232@gmail.com>
2023-12-11 12:55:06 +0500

    Challange Completed : Styled Text

--- dice_game/lib/main.dart
+++ dice_game/lib/main.dart
@@ -18,7 +18,10 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text("Hello World"),
+            child: Text(
+              "Hello World",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
           ),
         ),
       ),
