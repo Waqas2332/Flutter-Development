3579e00dffbee1938e30c6e52585b38767b49d27
waqas <wmunir232@gmail.com>
2023-12-30 14:33:53 +0500

    Practiced implemented resuability

--- dice_game/lib/gradient_container.dart
+++ dice_game/lib/gradient_container.dart
@@ -2,21 +2,19 @@ import 'package:dice_game/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
+  final List<Color> colors;
   // super runs the constructor function of parent class
   // key is required by StatelessWidget class
   // main purpose of constructor is to forward the key parameter to parent class
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
   // build method is required for custom widgets and it triggers by flutter whenever user interface is rendered
   // return type of build method is Widget
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98)
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
