70b8df9ca2db600747b69877709f14eaaf17a177
waqas <wmunir232@gmail.com>
2024-01-24 17:49:08 +0500

    Adding margin and padding to fix buttons

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -18,24 +18,30 @@ class _QuestionsScreen extends State<QuestionsScreen> {
     return SizedBox(
       // For giving as much width as available
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map(
-            (answer) {
-              return AnswerButton(() {}, answer);
-            },
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswerButton(() {}, answer);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
