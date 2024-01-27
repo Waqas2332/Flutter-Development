1a6ec63e01c2a80541fd1eadd4d784e764806590
waqas <wmunir232@gmail.com>
2024-01-27 18:21:10 +0500

    Build Result Screen

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/home.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,7 +14,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = "home-screen";
 
   void switchScreen() {
@@ -23,13 +25,24 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = "results-screen";
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var currScreen = activeScreen == "home-screen"
-        ? Home(switchScreen)
-        : QuestionsScreen(chooseAnswer);
+    Widget currScreen = Home(switchScreen);
+    if (activeScreen == "questions-screen") {
+      currScreen = QuestionsScreen(chooseAnswer);
+    }
+
+    if (activeScreen == "results-screen") {
+      currScreen = const ResultsScreen();
+    }
 
     return MaterialApp(
       home: Scaffold(
---
+++ quiz_app/lib/results_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // For giving as much width as available
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("You answers X out of Y questions correctly"),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text("List of answers & questions"),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Restart Quiz"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
