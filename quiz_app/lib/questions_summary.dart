0e3f54c057303504acb3fa21cb00c6146108c6d5
waqas <wmunir232@gmail.com>
2024-01-28 14:32:04 +0500

    Making List Scrollable

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -6,30 +6,36 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summary;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summary.map((data) {
-        return Row(
-          children: [
-            Text(
-              ((data["question_index"] as int) + 1).toString(), // type casting
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    data["question"].toString(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summary.map((data) {
+            return Row(
+              children: [
+                Text(
+                  ((data["question_index"] as int) + 1)
+                      .toString(), // type casting
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        data["question"].toString(),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(data["choosen_answer"] as String),
+                      Text(data["correct_answer"] as String),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(data["choosen_answer"] as String),
-                  Text(data["correct_answer"] as String),
-                ],
-              ),
-            ),
-          ],
-        );
-      }).toList(), // For converting iterable to a List
+                ),
+              ],
+            );
+          }).toList(), // For converting iterable to a List
+        ),
+      ),
     );
   }
 }
