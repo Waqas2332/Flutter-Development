193fd8c9d7995af65eac431789b93868ce289873
waqas <wmunir232@gmail.com>
2024-01-31 15:34:12 +0500

    Used Card And Spacer widget

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -10,8 +11,8 @@ class ExpensesList extends StatelessWidget {
     // ListView for long scrollable lists, .builder() is used to create items when they are about to be visible i.e, for showing items dynamically
     return ListView.builder(
       itemCount: expensesList.length,
-      itemBuilder: (ctx, index) => Text(
-        expensesList[index].title,
+      itemBuilder: (ctx, index) => ExpensesItem(
+        expensesList[index],
       ),
     );
   }
