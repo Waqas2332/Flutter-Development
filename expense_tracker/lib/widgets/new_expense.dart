4fe8ec008691257d27a21c34410b1b55cf6546f6
waqas <wmunir232@gmail.com>
2024-02-20 16:20:34 +0500

    Handled User input with text field

---
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpense();
+  }
+}
+
+class _NewExpense extends State<NewExpense> {
+  var _eneteredTitle = "";
+  void _saveTitleInput(String inputValue) {
+    _eneteredTitle = inputValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: _saveTitleInput,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text("Title"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_eneteredTitle);
+                  },
+                  child: const Text("Save Expense"))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
