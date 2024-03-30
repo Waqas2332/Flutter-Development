1ec338c98911b1f0aa83d4badfe5f131eecfbff9
waqas <wmunir232@gmail.com>
2024-03-30 17:27:16 +0500

    setup theme for appbar and whole app

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var colorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      colorScheme: colorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: colorScheme.onPrimaryContainer,
+          foregroundColor: colorScheme.primaryContainer),
+    ),
+    home: const Expenses(),
   ));
 }
